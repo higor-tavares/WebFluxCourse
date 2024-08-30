635cc695db2a3ed7051ac8409360ef5b108c5143
Higor Tavares <paulohtfreire@gmail.com>
2024-08-29 22:17:44 -0300

    feat: first commit

---
+++ src/test/java/com/higortavares/WebFluxCourse/WebFluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higortavares.WebFluxCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
