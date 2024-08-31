0377b13136632c9634e46419fa32099a8e81ad79
Higor Tavares <paulohtfreire@gmail.com>
2024-08-31 15:31:42 -0300

    chore: fix mapper

--- src/main/java/com/higortavares/WebFluxCourse/mapper/UserMapper.java
+++ src/main/java/com/higortavares/WebFluxCourse/mapper/UserMapper.java
@@ -5,8 +5,13 @@ import com.higortavares.WebFluxCourse.model.request.UserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
 @Mapper(
-        componentModel = "spring"
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = IGNORE,
+        nullValueCheckStrategy = ALWAYS
 )
 public interface UserMapper {
     @Mapping(target = "id", ignore = true)
